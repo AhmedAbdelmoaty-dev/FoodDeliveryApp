190357d6220d4716e9b5e5f9a781004a703bb1a0
ahmed abd elmoaty <test@example.com>
2025-12-10 22:33:02 +0200

    Structuring Project

--- API/Program.cs
+++ API/Program.cs
@@ -1,5 +1,19 @@
+using Application.Extensions;
+using Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices().AddInfrastructureServices(builder.Configuration);
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/", () => "Hello World!");
 
---
+++ Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            {
+                opt.UseNpgsql(configuration.GetConnectionString("DbConnection"));
+            });
+
+            return services;
+        }
+    }
+}
